58f3e31dc101d6c25646d7611ca0c3363d68a9e2
Mangesh2042 <mangesh2042@gmail.com>
2022-07-15 14:51:33 +0530

    demo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
+ function App() {
+const [data,setData]=useState([])
+const [search,setSearch]=useState("react")
+const [order,setOrder]=useState("asc")
 
-function App() {
+
+
+
+useEffect(()=>{
+  loadingData();
+},[])
+
+useEffect(()=>{
+  loadingData();
+},[order])
+
+const loadingData=async ()=>{
+ return await axios.get(`https://api.github.com/search/repositories?q=${search}&sort=${order}`).then((res)=>setData(res.data)).catch((err)=>console.log(err))
+}
+
+const submitHanddle=()=>{
+  loadingData();
+  //console.log(order);
+}
+
+
+const handleChange = (e) => {
+  setOrder(e.target.value);
+};
+
+
+
+
+ console.log(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <>
+     <div>
+        <input type="text" name="" id="" value={search} onChange={(e)=>setSearch(e.target.value)} />
+        <button onClick={submitHanddle}>search</button>
+        <select name="" id="" value={order} onChange={handleChange}>
+          <option value="asc">asc</option>
+          <option value="dec">dec</option>
+         </select>     
     </div>
+
+    <div>
+      {
+        <table>
+            <tr>
+              <td>name</td>
+              <td>language</td>
+              <td>size</td>
+              <td>visibility</td>
+              <td>watchers count</td>
+              <td>description</td>
+            </tr>
+            <tbody>
+              {
+              data?.items?.map((res)=>{
+              return( 
+              <>
+                <tr>
+                  <td>{res.name}</td>
+                  <td>{res.language}</td>
+                  <td>{res.size}</td>
+                  <td>{res.visibility}</td>
+                  <td>{res.watchers_count}</td>
+                  <td>{res.description}</td>
+                </tr>
+              </>
+              )
+              })
+              }
+            </tbody>
+          </table>
+        
+       
+      
+      
+      }
+    </div>
+     </>
   );
 }
 
